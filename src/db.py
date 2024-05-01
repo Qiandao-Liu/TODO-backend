d6897e2d656ad8fc3827f98676621776c590bdd2
alexmcgowan112 <alexhasswag112@gmail.com>
2024-05-01 11:45:31 -0400

    Add Dockerfile

--- src/db.py
+++ src/db.py
@@ -43,14 +43,14 @@ class Recipe(db.Model):
     # Can't make it a list unless we make it another table relationship (meaning that users have less flexibility)
     ingredients = db.Column(db.String, nullable=False)
     directions = db.Column(db.String, nullable=False)
-    owner = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    publisher_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     bookmarkers = db.relationship("User", secondary=saved_recipes_table, back_populates='bookmarked_recipes')
 
-    def __init__(self, title, ingredients, directions, owner, image=None, description=None):
+    def __init__(self, title, ingredients, directions, publisher_id, image=None, description=None):
         self.title = title
         self.ingredients = ingredients
         self.directions = directions
-        self.owner = owner
+        self.publisher_id = publisher_id
         self.image = image
         self.description = description
 
@@ -60,7 +60,7 @@ class Recipe(db.Model):
             "title": self.title,
             "ingredients": self.ingredients,
             "directions": self.directions,
-            "owner": self.owner.simple_serialize(),
+            "owner": self.publisher_id.simple_serialize(),
         }
         if self.description is not None:
             dic["description"] = self.description
